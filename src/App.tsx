56623b84e331e6f5ee970c88bb4eb5580bf491e5
Ayush Singh <asayushsingh80@gmail.com>
2024-09-22 16:28:43 +0530

    ui: svg animations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,29 @@
 import { PlayCircle } from "lucide-react";
 import Navbar from "./components/Navbar";
+import { SpaceShipSvg } from "./components/SVGcomponents/SpaceShipSVG";
+import { motion, Variants } from "framer-motion";
+
+const tshirtAnimation: Variants = {
+  animate: {
+    scale: [1, 1.05, 1],
+    transition: { duration: 5, repeat: Infinity },
+  },
+};
+
+const tshirtTextAnimation: Variants = {
+  animate: {
+    scale: [0, 0.9],
+    opacity: [0, 1],
+    transition: { duration: 0.5 },
+  },
+};
 
 function App() {
   return (
     <>
       <Navbar />
       <main className="max-w-full mx-auto mt-20">
-        <section className="flex flex-col mx-auto max-w-[1216px] py-14 px-6 h-screen justify-between">
+        <section className="flex flex-col mx-auto max-w-[1216px] py-14 px-6 h-screen gap-4">
           <div className="flex flex-col gap-6 max-w-80">
             <h1 className="text-3xl font-semibold pr-[27%]">
               Create and sell custom products
@@ -40,7 +57,23 @@ function App() {
               </span>
             </div>
           </div>
-          <div>h</div>
+          <div className="relative flex justify-center rounded-full w-80 h-80 bg-black-6">
+            <SpaceShipSvg className="z-50" />
+            <motion.div
+              className="absolute h-32 bottom-10"
+              variants={tshirtAnimation}
+              animate="animate"
+            >
+              <img src="/tshirt.png" className="h-32" />
+              <motion.h1
+                variants={tshirtTextAnimation}
+                animate="animate"
+                className="absolute w-16 font-semibold bottom-[40%] text-primary-1 left-[24%] text-center border-2 border-dashed border-primary-1 rounded"
+              >
+                Your Design
+              </motion.h1>
+            </motion.div>
+          </div>
         </section>
       </main>
     </>
